ba270cd6914fb40be66c667ff9f0322ff98eb9ec
arham-anees <arhamanees10@gmail.com>
2022-03-24 11:04:04 +0000

    cast vote method written

--- EVM updated/Vote.cs
+++ EVM updated/Vote.cs
@@ -10,10 +10,15 @@ namespace EVM_updated
     {
         private static int _Count;
         public int Id{get; private set;}
-        public Vote()
+        public int VoterId {get; private set;}
+        public int CandidateId {get; private set;}
+        public DateTime CastTime {get; private set;}
+        public Vote(int voterId, int candidateId)
         {
             this.Id=++_Count;
-               
+               VoterId=voterId;
+               CandidateId=candidateId;
+               CastTime=DateTime.UtcNow;
         }
     }
 }
