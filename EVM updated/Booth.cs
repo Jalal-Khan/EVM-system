66557ac19249e527866a3de335474f34c356ca1f
arham-anees <arhamanees10@gmail.com>
2022-03-24 11:04:12 +0000

    cast vote

--- EVM updated/Booth.cs
+++ EVM updated/Booth.cs
@@ -14,9 +14,9 @@ namespace EVM_updated
         List<Voter> VotersList = new List<Voter>();
         List<Vote> VotesList = new List<Vote>();
 
-public Booth(){
-    this.Id=++_Count;
-}
+        public Booth(){
+            this.Id=++_Count;
+        }
 
         public void AddVoter()
         {
@@ -59,7 +59,31 @@ public Booth(){
 
         public void VoteCast()
         {
-            Vote voteCast = new Vote();
+            Console.WriteLine("Enter your CNIC");
+            var cnic=Console.ReadLine();
+            // check if voter is registered in this booth or not
+            Voter voter=_FindVoterByCnic(cnic);
+            while(voter==null){
+                Console.WriteLine("Invalid CNIC or Voter is not enlisted in this Booth");
+                cnic=Console.ReadLine();
+                voter=_FindVoterByCnic(cnic);
+            }
+            // check if voter has casted vote or not
+            if(_IsVoteCasted(voter.Id)){
+                Console.WriteLine("You have already casted vote. You cannot cast another vote.");
+                return;
+            }
+
+            Console.WriteLine("Enter your Candidate Sign");
+            var sign=Console.ReadLine();
+            Candidate candidate=_FindVoterByCnic(sign);
+            while(voter==null){
+                Console.WriteLine("Invalid Sign. Please select a valid sign");
+                sign=Console.ReadLine();
+                candidate=_FindVoterByCnic(sign);
+            }
+
+            Vote voteCast = new Vote(voter.Id, candidate.Id);
             VotesList.Add(voteCast);
         }
         public void VoterMenu()
@@ -75,5 +99,36 @@ public Booth(){
         }
 
 
+        // finds voter by CNIC in voters list
+        private Voter _FindVoterByCnic(string cnic){
+            Voter voter=null;
+            for(int i=0;i<VotersList.Count;i++){
+                if(VotersList[i].CNIC==cnic){
+                    voter=VotersList[i];
+                    break;
+                }
+            }
+            return voter;
+        }
+
+        // finds candidate by Sign in candidates list
+        private Voter _FindCandidateBySign(string sign){
+            Candidate candidate=null;
+            for(int i=0;i<CandidateList.Count;i++){
+                if(CandidateList[i].Sign==sign){
+                    candidate=CandidateList[i];
+                    break;
+                }
+            }
+            return candidate;
+        }
+
+        // checks whether vote is casted by voter or not
+        private bool _IsVoteCasted(int id){
+            for(int i=0;i<VotersList.Count;i++){
+                if(VotersList[i].Id==id)return true;
+            }
+            return false;
+        }
     }
 }
